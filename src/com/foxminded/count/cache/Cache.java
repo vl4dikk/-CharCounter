53ae5b4ad5870f319fb0f7b367f23cef28db3d46
Valhuk <vladrockyvlad@gmail.com>
2022-06-21 11:28:12 +0300

    moved realCountProcessor to fields of ProxyCountProcessor

--- src/com/foxminded/count/cache/Cache.java
+++ src/com/foxminded/count/cache/Cache.java
@@ -4,20 +4,20 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Cache {
-	
-	private Map <String, Map<Character, Integer>> cache;
-	
-	
+
+	private Map<String, Map<Character, Integer>> cache;
 
 	public Cache(int size) {
-		this.cache = new LinkedHashMap<String, Map<Character,Integer>>(size);
+		this.cache = new LinkedHashMap<String, Map<Character, Integer>>(size);
 	}
+
 
 	public Map<String, Map<Character, Integer>> getCache() {
 		return cache;
 	}
 
-	public  Map<Character, Integer> set (String input, Map<Character, Integer> mapAfterProcessor){
+
+	public Map<Character, Integer> set(String input, Map<Character, Integer> mapAfterProcessor) {
 		return this.cache.put(input, mapAfterProcessor);
-	}		
+	}
 }
--- src/com/foxminded/count/processor/ProxyCountProcessor.java
+++ src/com/foxminded/count/processor/ProxyCountProcessor.java
@@ -5,16 +5,18 @@ import java.util.Map;
 import com.foxminded.count.cache.Cache;
 
 public class ProxyCountProcessor implements CountProcessor {
+	
+	RealCountProcessor processor = new RealCountProcessor();
 
 	@Override
 	public Map<Character, Integer> count(Cache cache, String input) {
 		if(cache.getCache() == null) {
-			return new RealCountProcessor().count(cache, input);
+			return processor.count(cache, input);
 		}
 	else if(cache.getCache().containsKey(input)) {
 			return cache.getCache().get(input);
 		}else 
-			return new RealCountProcessor().count(cache, input);
+			return processor.count(cache, input);
 	}
 
 }
